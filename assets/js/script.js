1947f7ca8a5ff98fd80fa19f025dd1b26b46018f
antoniostassi <stassiant@outlook.it>
2024-07-17 17:42:35 +0200

    Initialising Repo

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+console.log("Hela");
+let imageList = [
+    "assets/img/01.webp",
+    "assets/img/02.webp",
+    "assets/img/03.webp",
+    "assets/img/04.webp",
+    "assets/img/05.webp",
+];
+
+const prvsButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+let imgTag = document.querySelector("img");
+
