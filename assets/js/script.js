c60835230016fb5c01f87e0bd776a1830d57614f
antoniostassi <stassiant@outlook.it>
2024-07-17 17:42:48 +0200

    JS | Buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,28 @@ const prvsButton = document.getElementById("previous");
 const nextButton = document.getElementById("next");
 let imgTag = document.querySelector("img");
 
+let i = 0;
+
+prvsButton.addEventListener("click", function() {
+    if (i == 0) {
+        i = 4;
+    } else {
+        i--;
+    }
+    console.log(imageList[i]);
+    imgTag.outerHTML = '<img src=' + '"' + imageList[i] + '">'
+    imgTag = document.querySelector("img");
+})
+
+nextButton.addEventListener("click", function() {
+
+    if (i == 4) {
+        i = 0;
+    } else {
+        i++;
+    }
+    console.log(imageList[i]);
+    imgTag.outerHTML = '<img src=' + '"' + imageList[i] + '">'
+    imgTag = document.querySelector("img");
+
+})
